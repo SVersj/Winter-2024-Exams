2600ec9de713701b582d7d3b437bb23482cd4ce8
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 22:46:29 +0200

    5.2

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,12 +1,12 @@
 // Generate random password
-//Step 1:
-// Use const to declare variables and add missing let keywords for key and Index variables:
+//Step 2:
+// Use a short form of the assignment operator to concatenate strings
 let GeneratePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
     let index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[index];
+    key += alphabet[index];
   }
   return key;
 };
