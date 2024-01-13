27db5b3541dbcf4549e0d6b6f49c74309ac623f4
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 20:56:41 +0200

    2.3

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,23 +1,15 @@
 // Get day number
-// Step 2:
-// Move the dayIndex variable declaration to the beginning of the function to improve code readability
+// Step 3:
+// We can use the findIndex method to improve readability and reduce code.
 
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (inputString) => {
-  let dayIndex;
+  const dayIndex = daysOfWeek.findIndex(day => inputString.startsWith(day.toLowerCase()));
 
-  for (dayIndex = 0; dayIndex < daysOfWeek.length; dayIndex++) {
-    if (inputString.startsWith(daysOfWeek[dayIndex].toLowerCase())) {
-      return dayIndex + 1;
-    }
-  }
-
-  return -1;
+  return dayIndex !== -1 ? dayIndex + 1 : -1;
 };
 
 module.exports = getDayNumber;
-
-
 
 
