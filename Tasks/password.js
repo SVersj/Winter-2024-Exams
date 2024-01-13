da5277cc7e18928a2c177e194be5e92d2e1a00af
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 22:51:06 +0200

    5.4

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,7 +1,8 @@
 // Generate random password
-//Step 3:
-// Rename the key and index variables for clarity
-let GeneratePassword = (alphabet, length) => {
+//Step 4:
+//Enter default parameters for the GeneratePassword function
+// to make it easier to call the function without explicitly specifying the password length:
+let GeneratePassword = (alphabet, length = 8) => {
   const MAX = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
