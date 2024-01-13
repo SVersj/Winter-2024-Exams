a9dbc4ab68c7b171738a02e471d3334f115a9fe3
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 23:46:57 +0200

    7.2

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,18 +1,19 @@
 // Generate int array from given range
-// Step 1:
-// Rename the function and use destructuring to improve readability
+// Step 2:
+// Check the condition at the beginning of the function and return an empty array immediately
+// if to is less than from
 const createRange = (...range) => {
   let [from, to] = range;
-  if (to >= from) {
-    range = new Array(to - from + 1);
-    for (let i = from; i <= to; i++) {
-      range[i - from] = i;
-    }
-  } else {
+  if (to < from) {
     return [];
+  }
+  range = new Array(to - from + 1);
+  for (let i = from; i <= to; i++) {
+    range[i - from] = i;
   }
   return range;
 };
 
 module.exports = createRange;
+
 
