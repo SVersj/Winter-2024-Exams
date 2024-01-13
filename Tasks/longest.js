62d43e4bc6376918dc55c8d9d416894cf7ae858a
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 21:30:58 +0200

    3.2

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,30 +1,18 @@
 // Find longest string
-// Step 1:
-// Use const to declare variables that will not change,
-// and add missing let keywords for variables x and s
-const longest = function(line = []) {
-  let x = -1;
-  let s = ['Not found'][0][x++];
-  for (let i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+// Step 2:
+// improve variable names and logic
+const longest = function(lines = []) {
+  let maxLength = -1;
+  let longestLine = ['Not found'][0][maxLength++];
+  for (let line of lines) {
+    if (line.length > maxLength) {
+      maxLength = line.length;
+      longestLine = line;
     }
   }
-  Object.assign(line, { length: s.length });
-  return s;
+  Object.assign(lines, { length: maxLength });
+  return longestLine;
 };
 
 module.exports = longest;
+
