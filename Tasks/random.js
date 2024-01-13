c13428970cfab5e0c736147fa7d54d08cab15ae6
SVersj <86892022+SVersj@users.noreply.github.com>
2024-01-13 23:34:37 +0200

    6.2

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,13 +1,6 @@
 // Generate random integer value in given range
-//Step 1:
-// Improve function naming and parameters
-const getRandomNumber = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-};
+//Step 2:
+// Using the Arrow Function for a More Concise Syntax
+const getRandomNumber = (min, max = min) => Math.floor(Math.random() * (max + 1));
 
 module.exports = getRandomNumber;
